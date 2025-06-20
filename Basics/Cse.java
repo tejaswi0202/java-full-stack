111513540958234f8aade9240b63a0106ce3f952
tejaswi0202 <ejjutejaswi@gmail.com>
2025-06-20 14:47:13 +0530

    Add files via upload

---
+++ Basics/Cse.java
@@ -0,0 +1,111 @@
+/*public class Default
+{
+int i;
+float f;
+double d;
+char c;
+String s;
+short sh;
+byte b;
+boolean bo;
+long l;
+public static void main(String args[])
+{
+Default a=new Default();
+System.out.println("int value:"+a.i);
+System.out.println("float value:"+a.f);
+System.out.println("double value:"+a.d);
+System.out.println("char value:"+a.c);
+System.out.println("string value:"+a.s);
+System.out.println("short value:"+a.sh);
+System.out.println("byte value:"+a.b);
+System.out.println("boolean value:"+a.bo);
+System.out.println("long value:"+a.l);
+}
+}
+output:
+int value:0
+float value:0.0
+double value:0.0
+char value:
+string value:null
+short value:0
+byte value:0
+boolean value:false
+long value:0
+
+class Cse{
+void student()
+{
+System.out.println("Name:kalyani");
+System.out.println("class:10");
+}
+public static void main(String[] args)
+{
+Cse c=new Cse();
+c.student();
+}
+}
+output:
+Name:kalyani
+class:10
+
+
+
+class Name
+{
+public static void main(String[] args){
+System.out.println("tejaswi");
+}
+}
+output:
+tejaswi
+
+public class Variables
+{
+ int a=20;
+public static void main(String[] args)
+{
+Variables c=new Variables();
+int a=30;
+System.out.println(c.a);
+
+System.out.println(a);
+}
+}
+output:
+20
+30Tejaswi
+
+class Name
+{
+void name()
+{
+System.out.println("Tejaswi");
+}
+public static void main(String[] args)
+
+{
+Name c=new Name();
+c.name();
+}
+}
+output:
+Tejaswi
+*/
+class Comments
+{
+public static void main(String[] args){
+// this is a single line comment
+System.out.println("//this is single line comment");
+/* this is a multi line comment
+welcome to java programming
+*/
+System.out.println("/*this is multiple line comment*/");
+System.out.println("/**this is document comment*/");
+}
+}
+output:
+//this is single line comment
+/*this is multiple line comment*/
+/**this is document comment*/
